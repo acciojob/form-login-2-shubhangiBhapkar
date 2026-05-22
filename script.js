040bab53c12abd0cde298a5529b280a5e5d91424
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-22 12:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+//your JS code here. If 
+const form = document.getElementById("sign-up");
+const para = document.getElementById("op");
+form.addEventListener("submit", ()=>{
+	
+	const fname = document.getElementById("fname");
+	const lname = document.etElmentById("lname");
+	const phn = document.getElementById("pno");
+	const eid = document.getElementById("Eid");
+
+	 op.innerHTML = "<h1>hello<h1>"
+	//"First Name"+ fname + " Phone Number:"+ p;hn +  "Email ID:"+ eid);
+	
+	// alert("First Name"+ fname + " Phone Number:"+ phn +  "Email ID:"+ eid)
+});
