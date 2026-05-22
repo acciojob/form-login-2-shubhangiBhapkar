a51a5cf480953a19a416ff0e8754b739ec84cc02
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-22 12:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-//your JS code here. If 
-const form = document.getElementById("sign-up");
-const para = document.getElementById("op");
-form.addEventListener("submit", ()=>{
-	
-	const fname = document.getElementById("fname");
-	const lname = document.etElmentById("lname");
-	const phn = document.getElementById("pno");
-	const eid = document.getElementById("Eid");
+const form = document.querySelectorAll("#sign-up");
+const para = document.querySelectorAll("#op");
 
-	 op.innerHTML = "<h1>hello<h1>"
-	//"First Name"+ fname + " Phone Number:"+ p;hn +  "Email ID:"+ eid);
-	
-	// alert("First Name"+ fname + " Phone Number:"+ phn +  "Email ID:"+ eid)
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const fname = document.querySelectorAll("#fname").value;
+    const lname = document.querySelectorAll("#lname").value;
+    const phn = document.querySelectorAll("#pno").value;
+    const eid = document.querySelectorAll("#Eid").value;
+
+    para.innerHTML = `
+        First Name: ${fname}<br>
+        Last Name: ${lname}<br>
+        Phone Number: ${phn}<br>
+        Email ID: ${eid}
+    `;
 });
